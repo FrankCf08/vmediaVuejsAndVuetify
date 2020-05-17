acfda3364365ea334277b81f0988793652560042
Frank Cruz <frankcruz0806@gmail.com>
2020-05-16 20:58:35 -0500

    Removing Global Mixins Temporarily
    *THis is creating alert calls for each click

--- src/mixins/formHelperClass55.js
+++ src/mixins/formHelperClass55.js
@@ -1,18 +1,18 @@
 import Vue from 'vue'
 
 // Global Mixin: Thi: VUe.mixin() is called for every instance created App.Vue, Class55, etc
-Vue.mixin({
- created(){
-  console.info('Hi from the global mixin');
+// Vue.mixin({
+//  created(){
+//   console.info('Hi from the global mixin');
   
-  this.greet(this.$options.AppName)  
- },
- methods:{
-  greet(name){
-   alert(`Hi from ${name}`)
-  }
- }
-})
+//   this.greet(this.$options.AppName)  
+//  },
+//  methods:{
+//   greet(name){
+//    alert(`Hi from ${name}`)
+//   }
+//  }
+// })
 
 // Local Mixim
 export default {
